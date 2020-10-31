ca81f3411892a7a8839a0625c508ea937cd2c628
jwatt80 <jwatt@bu.edu>
2020-10-31 15:14:41 -0500

    adds _train data sets into train_merge

--- run_analysis.R
+++ run_analysis.R
@@ -11,3 +11,12 @@ y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
 
 test_merge <- data.frame("subject" = subject_test[,1],
                          "ytest" = y_test[,1])
+
+#Does the same for train data sets
+X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
+
+train_merge <- data.frame("subject" = subject_train[,1],
+                         "ytest" = y_train[,1])
+
